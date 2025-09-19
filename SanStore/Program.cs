513dd380058993ffc17a5020889259d370d5efc1
alasaneeth <alasaneeth22@gmail.com>
2025-09-19 19:36:36 +0300

    cors configration

--- SanStore/Program.cs
+++ SanStore/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using SanStore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options=>
+{
+    options.AddPolicy("CustomePolicy", x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+#region Database Connectivity
+builder.Services.AddDbContext<ApplicationDbContext>(options=> 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+#endregion End of Database Connectivity
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CustomePolicy");
 
 app.UseHttpsRedirection();
 
