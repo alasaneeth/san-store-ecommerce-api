fa7b4bad567335a20288e38f5a9dfb4e58fe8a36
alasaneeth <alasaneeth22@gmail.com>
2025-09-21 13:23:05 +0300

    Create Custom api response

---
+++ SanStore.Domain/Common/APIResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanStore.Domain.Common
+{
+    public class APIResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = false;
+        public Object Result { get; set; }  
+        public string DisplayMessage { get; set; }
+        public List<APIError> Errors { get; set; }
+        public List<APIWarning> Warnings { get; set; }
+
+
+        public void AddError(string errorMessage)
+        {
+            APIError error = new APIError(description: errorMessage);
+            Errors.Add(error);
+        }
+
+        public void AddWarning(string warningMessage)
+        {
+            APIWarning error = new APIWarning(description: warningMessage);
+            Warnings.Add(error);
+        }
+
+    }
+
+
+
+}
---
+++ SanStore.Domain/Common/APIWarning.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanStore.Domain.Common
+{
+    public class APIWarning
+    {
+        public string Description { get; set; }
+
+        public APIWarning(string description)
+        {
+            Description = description;
+        }
+
+    }
+}
