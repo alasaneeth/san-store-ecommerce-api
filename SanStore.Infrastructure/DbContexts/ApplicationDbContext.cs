6fea5168f13b420257e0aef3abd51944a2a5ecb9
alasaneeth <alasaneeth22@gmail.com>
2025-09-26 12:01:17 +0300

    Install and configure Entity framwork core Identity

--- SanStore.Infrastructure/DbContexts/ApplicationDbContext.cs
+++ SanStore.Infrastructure/DbContexts/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SanStore.Domain.Models;
 using System;
 using System.Collections.Generic;
@@ -8,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace SanStore.Infrastructure.DbContexts
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
--- SanStore/Program.cs
+++ SanStore/Program.cs
@@ -2,6 +2,7 @@ using SanStore.Infrastructure;
 using SanStore.Application;
 using Microsoft.EntityFrameworkCore;
 using SanStore.Infrastructure.DbContexts;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,12 @@ builder.Services.AddCors(options=>
 #region Database Connectivity
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser,IdentityRole>(options=>
+{
+
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
 #endregion End of Database Connectivity
 
 builder.Services.AddControllers();
